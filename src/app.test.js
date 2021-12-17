bc4276930848cd0eae697e4de981f82624517422
kamilacc97 <93764155+kamilacc97@users.noreply.github.com>
2021-12-17 14:51:03 +0100

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -3,5 +3,5 @@ const { dayOfTheWeek } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('12/17/2021') );
-    expect( day ).toBe('Friday');
+    expect( day ).toBe('Wednesday');
 });
